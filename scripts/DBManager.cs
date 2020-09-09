008cadce5f16fd3183e976f3f8815f4276824bac
shanshan <lishanshan.unnc@aiesec.net>
2020-09-09 22:33:00 +0800

    Code for saving current IPD and current round

---
+++ scripts/DBManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DBManager
+{
+    public static string username;
+    public static int currentIPD;
+    public static int currentCount;
+    public static List<int> testIPD;
+    public static List<int> randomScene;
+}
