d7487fddb7059b37951713d260d41e02e04dfc85
shanshan <lishanshan.unnc@aiesec.net>
2020-09-09 22:34:37 +0800

    Code for generating random name

---
+++ scripts/generateRandomName.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class generateRandomName : MonoBehaviour
+{
+
+    /*********************************************************************
+     * Generate a random alphanumeric strings to identify the test result
+     *********************************************************************/
+    private static System.Random random = new System.Random();
+
+    public static string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+          .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // generate a random name
+        DBManager.username = RandomString(8);
+    }
+}
