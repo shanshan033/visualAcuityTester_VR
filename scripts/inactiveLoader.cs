eea732670ed4242f1d2e21cd45ed0198b7fdf9b4
shanshan <lishanshan.unnc@aiesec.net>
2020-09-09 22:35:09 +0800

    Inactive a loader

---
+++ scripts/inactiveLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inactiveLoader : MonoBehaviour
+{
+    public GameObject loader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        loader.SetActive(false);
+        
+    }
+
+}
